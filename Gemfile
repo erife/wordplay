f965024486e86fb006eac5c0d67558c2cec56ad3
Elaina Rife <elaina.rife@gmail.com>
2013-03-20 10:52:34 -0700

    ruby server

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org/"
+
+gem 'sinatra-contrib', path: 'C:\Users\Elaina\Documents\GitHub\sinatra-contrib'
+gem 'json'
+gem 'multi_json'
---
+++ server.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'multi_json'
+
+set :static => true
+set :public_folder, File.dirname(__FILE__) + 'public'
+puts '==================ER'
+puts File.dirname(__FILE__) + 'public'
+
+WORDS = 
+%w(
+fat
+man
+net 
+rig
+fast 
+high
+faster 
+higher 
+magnet 
+)
+
+get "/" do
+erb :index
+end
+
+get "/data" do
+  json :words => WORDS, :availableletters => %w(f a s t e r)
+end
+
