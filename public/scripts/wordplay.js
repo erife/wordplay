43a1bd8993850384929cb8398052903e7a1b67fc
Elaina Rife <elaina.rife@gmail.com>
2013-04-04 18:19:29 -0700

    fixed win/lose

--- public/scripts/wordplay.js
+++ public/scripts/wordplay.js
@@ -8,117 +8,6 @@ function initialword(words)
     return $.grep(words, function(word,i){return word.length == maxwordlength;})[0];
 }
 
-function subwords(availableletters, words)
-{
-    return $.grep(words, function(word){
-	var myavailable = availableletters.slice();
-	var letters = word.split("");
-	for(var x = word.length-1; x >= 0; x--){
-	    var letter = letters.pop();
-	    var foundpos = myavailable.indexOf(letter);
-	    if(foundpos  == -1)
-	    {
-		return false;
-	    }
-	    else{
-		myavailable.splice(foundpos, 1);
-	    }
-	    
-	}
-	return true;
-    });
-}
-
-
-// var words = [
-//     "after",
-//     "are",
-//     "art",
-//     "arts",
-//     "ate",
-//     "ear",
-//     "ears",
-//     "east",
-//     "eat",
-//     "eats",
-//     "far",
-//     "fare",
-//     "fares",
-//     "farts",
-//     "fast",
-//     "faster",
-//     "fat",
-//     "fate",
-//     "fates",
-//     "fear",
-//     "fears",
-//     "feast",
-//     "feat",
-//     "fret",
-//     "par",
-//     "pare",
-//     "pares",
-//     "parse",
-//     "part",
-//     "parts",
-//     "past",
-//     "paste",
-//     "pat",
-//     "pats",
-//     "pea",
-//     "pear",
-//     "pears",
-//     "peas",
-//     "pert",
-//     "pest",
-//     "pet",
-//     "pets",
-//     "raft",
-//     "rafts",
-//     "rap",
-//     "rapt",
-//     "rat",
-//     "rate",
-//     "rates",
-//     "rats",
-//     "ref",
-//     "rep",
-//     "rest",
-//     "safe",
-//     "safer",
-//     "sap",
-//     "sat",
-//     "sea",
-//     "sear",
-//     "seat",
-//     "set",
-//     "spa",
-//     "spar",
-//     "spare",
-//     "spat",
-//     "spear",
-//     "star",
-//     "stare",
-//     "step",
-//     "strafe",
-//     "strap",
-//     "tap",
-//     "tape",
-//     "taper",
-//     "tapers",
-//     "tapes",
-//     "tar",
-//     "tarp",
-//     "tarps",
-//     "tea",
-//     "tear",
-//     "tears",
-//     "trap",
-//     "traps",
-// ];
-
-
-
 
 function validguess(guessword, words)
 {
@@ -135,8 +24,6 @@ function validguess(guessword, words)
 function fillfound(words)
 {
     $.map(words, function(word, i){
-	// var word_id = "word_" + i;
-	// $("#found").append("<li> <ul id='" + word_id + "'> </ul> </li>");
 	fillsingle(word, i);
     });
 }
@@ -255,6 +142,7 @@ function submitword(guessword){
 function wincheck(){
  if($(document).data("score") > $(document).data("winscore")){
 	$("#kitten").removeClass("invisible");
+     $(document).data("win", true);
 		}
 }
 
@@ -294,9 +182,12 @@ else{
 }
 
 function endgame(){
-    $(".kitty").attr("src", "/images/angrycat.jpeg");
-    $("#kitten").removeClass("invisible");
-    playsound("beep-2.mp3");
+    if(!$(document).data("win")){
+	$(".kitty").attr("src", "/images/angrycat.jpeg");
+	$("#kitten").removeClass("invisible");
+	console.log("lose");
+	playsound("beep-2.mp3");
+    }
 }
 
  function playsound(soundname){
@@ -314,6 +205,7 @@ function endgame(){
 $(function () {
     $.getJSON("data",function(result){
 	var resultwords = result["result_words"]
+	$(document).data("win", false);
 	$(document).data("availableletters", result["availableletters"]);
 	$(document).data("resultwords", resultwords);
 	$(document).data("remainingletters", result["availableletters"].slice(0));
@@ -321,13 +213,13 @@ $(function () {
 	fillfound(resultwords);
 	showletters("#letter", $(document).data("remainingletters"));
 	$(document).data("guessword", []);
-	$(document).data("score", 0);
+	$(document).data("score", 45);
 	$("#dynamicscore").html($(document).data("score"));
 	$(document).data("foundwords", []);
 	displayfound();
 	displayrem();
 	$(document).keydown(handlekey);
-	$(document).data("winscore", 50);
+	$(document).data("winscore", 0);
 	$(document).data("time", 300);
 	timedisplay($(document).data("time"));
 	var time = setInterval(timecountdown, 1000);
