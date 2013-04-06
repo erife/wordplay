7e1eaae0ad4aca2a6e079120e2f9ba0ecb1ed0d2
Elaina Rife <elaina.rife@gmail.com>
2013-04-05 20:31:51 -0700

    sped up ruby, made space for 120 words, capped subwords

--- server.rb
+++ server.rb
@@ -1,33 +1,24 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'multi_json'
+require 'set'
 
 set :static => true
 set :public_folder, File.expand_path(File.dirname(__FILE__) + '/public')
 
 MAXWORDLENGTH = 6;
-ALLWORDS = File.new("TWL_2006_ALPHA.txt").to_a.map{|word| word.chomp}
+ALLWORDS = Set.new(File.new("TWL_2006_ALPHA.txt").to_a.map{|word| word.chomp})
 SEED_WORDS = ALLWORDS.select{|word| word.length == MAXWORDLENGTH}
-
-def valid_subword(available_letters, word)
-  letters = word.split("")
-  my_available_letters = available_letters.dup
-  return false if word.length > available_letters.size || word.length < 3
-  letters.each do |letter|
-    if idx = my_available_letters.index(letter)
-      my_available_letters.delete_at(idx)
-    else 
-      return false
-    end
-  end
-  return true
-end
 
 def get_resultwords()
   seed_word = SEED_WORDS.sample(1).first
   available_letters = seed_word.split("")
-  subwords = ALLWORDS.select do |word|
-    valid_subword(available_letters, word)
+  permutation_words = []
+  (2..available_letters.length).each do |n|
+    permutation_words += available_letters.permutation(n+1).to_a.map(&:join)
+  end
+  subwords = permutation_words.select do |word|
+    ALLWORDS.include?(word)
   end
   return [available_letters, subwords]
 end
@@ -38,7 +29,7 @@ end
 
 get "/data" do
   resultwords = [];
-  while resultwords.length < 20 do
+  while resultwords.length <= 20 || resultwords.length >= 120 do
     available_letters,  resultwords = get_resultwords()
   end
   available_letters = available_letters.shuffle
