f12da05dd5abb6bc69e8c304b5cee5d70ec5419c
Elaina Rife <elaina.rife@gmail.com>
2013-08-17 22:15:46 -0700

    listen to functions

--- public/scripts/wordplay.js
+++ public/scripts/wordplay.js
@@ -6,6 +6,18 @@ $(function(){
     };
     
     var Message = Backbone.Model.extend({
+
+	defaults: function() {
+	    return {
+		message: "welcome to wordplay",
+		status: "worried",
+		threat: "neutral"
+	    };
+	},
+	
+	initialize: function(app) {
+	    this.listenTo(app,"change:state", this.setState);
+	},
 	
 	states: {
 	    pending: {
@@ -18,7 +30,16 @@ $(function(){
 		status: "happy",
 		threat: "low"
 	    }
+	},
+
+	setState: function(event) {
+	    var state=this.states[event.changed.state];
+	    this.set("message", state["message"]);
+	    this.set("status", state["status"]);
+	    this.set("threat", state["threat"]);
 	}
+
+
     });		
 
     var MessageView = Backbone.View.extend({
@@ -26,54 +47,77 @@ $(function(){
 	el: $("#messaging"),
 
 	messageTemplate: _.template($('#message-template').html()),
-	model: new Message(),
-	    
+	
+	
 
-	initialize: function() {
-	    this.state = this.model.states["pending"];
+	initialize: function(app) {
+	    this.model = new Message(app);	
+	    this.listenTo(this.model, "change", this.render);
 	    this.render();
 	    
 	},
 	
 	render: function() {
 	    var context={
-		message: this.state["message"], 
-		status: this.state["status"], 
-		threat: this.state["threat"]
+		message: this.model.get("message"),
+		status: this.model.get("status"),
+		threat: this.model.get("threat")
 	    }
 	    this.$el.html(this.messageTemplate(context));
+	}
+
+    });
+
+    var App = Backbone.Model.extend({
+	states: [
+	    "pending",
+	    "running",
+	    "finished"
+	],
+	
+	defaults: function(){
+	    return{
+		state: "pending"
+	    }
 	},
 
-	setState: function(state) {
-	    this.state = this.model.states[state];
-	    this.render();
-	}	    
+	nextLevel: function() {
+	    switch(this.get("state")){
+	    case "pending": this.set("state", "running");
+		break;
+	    case "running": 
+		console.log("check can level");
+		console.log("emit level event");
+		break;
+	    }
+	}
     });
+
     var AppView = Backbone.View.extend({
 	
 	el: $(document), 
 
+	model: new App(),
+
 	events: {
 	    "keydown":  "handleKeydown"
 	    },
+	
 
 	initialize: function() {
-	    this.messaging = new MessageView();
+	    this.messaging = new MessageView(this.model);
 	    this.render();
 	},
 
 
 	handleKeydown: function(event) {
 	    switch(event.which){
-		case 16: this.start();
+		case 16: this.model.nextLevel();
 		break;
 	    }
 
-	},
-	
-	start: function() {
-	    this.messaging.setState("running");
 	}
+	
 
     });
     
