207d9f8ec6c9ff2234e44e15ca830df0f5df2779
Elaina Rife <elaina.rife@gmail.com>
2014-08-26 20:18:20 -0700

    Fetched words into columns

--- public/scripts/wordplay_backbone.js
+++ public/scripts/wordplay_backbone.js
@@ -43,9 +43,19 @@ $(function(){
 	
 	model: Word, 
 	url: '/wordlist'
+    });
+    
+    var WordView = Backbone.View.extend({
+	
+	tagName: "li",
+	
+	template: _.template("<%= word %>"),	
 
-	
-	
+	render: function(){
+	    this.$el.html(this.template(this.model.toJSON()));
+	    return this;
+	}
+
     });
 
     var Words = new WordList;
@@ -56,11 +66,24 @@ $(function(){
 	
 	initialize: function(){
 	    this.letterGenerate("PUPPY");
-	    Words.fetch();
+	    Words.fetch({success: this.render});
 	},
 
 	render: function(){
-	 },
+	    
+	    Words.each(function(word, i){
+		var wordview = new WordView({model: word});
+		var column_id = "column"+Math.floor(i/10);
+		if(i%10 == 0){
+		    var column_li = $("<li>");
+		    column_ul = column_li.append($("<ul>", {id: column_id}));
+		    $('#found').append(column_li);
+		}
+		$("#"+column_id).append(wordview.render().el);
+		
+	    });
+	    
+	},
 
 	letterGenerate: function(letterSet){
 	    var app = this;
